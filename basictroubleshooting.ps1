b346c985469240598005b50bc03162358e502ffd
lithdk <37413497+lithdk@users.noreply.github.com>
2019-10-21 17:22:33 +0200

    Update basictroubleshooting.ps1

--- basictroubleshooting.ps1
+++ basictroubleshooting.ps1
@@ -10,6 +10,15 @@ $ledomaintrust = Test-ComputerSecureChannel
 $lelogonserver = $env:LOGONSERVER
 $legpresult = gpresult /r | out-string
 }
+$daysup = $leuptimecalc.days
+$msgForInput = "System uptime is greater than " + $daysup + " days. Turning off Fast Start-Up and rebooting might fix your problem. Do you want to open Power Options? (Y/N)"
+$registryPath = "HKCU:\SOFTWARE\Microsoft\Office\15.0\Common\Identity"
+$adalkey = "EnableADAL"
+$versionkey = "Version"
+
+
+
+
 
 Write-Host "`n`n ----------Copy From Here----------"
 Write-Host $leuptimeout
@@ -29,18 +38,63 @@ Write-Host "`nGPresult:"
 Write-Host $legpresult
 }
 
-$daysup = $leuptimecalc.days
-if ($daysup -ge 1) {
-$msgForInput = "System uptime is greater than " + $daysup + " days. Turning off Fast Start-Up and rebooting might fix your problem. Do you want to open Power Options?"
-$msgBoxInput =  [System.Windows.MessageBox]::Show($msgForInput,'Input Box','YesNo','Error')
-    switch  ($msgBoxInput) {
-    'Yes' {
-    write-host "You've selected 'Yes'. Remember to reboot after turning off Fast Start-Up"
-    control /name Microsoft.PowerOptions /page pageGlobalSettings
-    }
-    'No' {
-    write-host "You've selected 'no'."
-    }
-    }
 
+if ($daysup -ge 0) {
+    do { $myInput = (Read-Host $msgForInput).ToLower() } while ($myInput -notin @('y','n'))
+    if ($myInput -eq 'y') {
+        write-host "You've selected 'Yes'. Remember to reboot after turning off Fast Start-Up"
+        control /name Microsoft.PowerOptions /page pageGlobalSettings
+    } else {
+        write-host "You've selected 'No'."
+    }
+ 
 }
+
+
+write-host "`n`n--- MENU ---"
+write-host "Enter your selection, 9 to quit"
+write-host "1. Enable ADAL"
+write-host "2. Disable ADAL"
+write-host "3. Download teamviewer to desktop"
+write-host "`n9. Exit`n"
+
+do {
+ $answer = read-host "Please Make a Selection"  
+
+        #Enable adal
+        if ($answer -eq '1') {
+            echo "You selected $($answer). Enabling ADAL"
+            IF(!(Test-Path $registryPath))
+              {
+                New-Item -Path $registryPath -Force | Out-Null
+                New-ItemProperty -Path $registryPath -Name $adalkey -Value "1" -PropertyType DWORD -Force | Out-Null
+                New-ItemProperty -Path $registryPath -Name $versionkey -Value "1" -PropertyType DWORD -Force | Out-Null
+               }
+             ELSE {
+                New-ItemProperty -Path $registryPath -Name $adalkey -Value "1" -PropertyType DWORD -Force | Out-Null
+                New-ItemProperty -Path $registryPath -Name $versionkey -Value "1" -PropertyType DWORD -Force | Out-Null
+                }
+        }
+        #Disable adal
+        elseif ($answer -eq '2') {
+            echo "You selected $($answer). Disabling ADAL"
+            IF(!(Test-Path $registryPath))
+              {
+                New-Item -Path $registryPath -Force | Out-Null
+                New-ItemProperty -Path $registryPath -Name $adalkey -Value "0" -PropertyType DWORD -Force | Out-Null
+                Remove-ItemProperty -Path $registryPath -Name $versionkey
+               }
+             ELSE {
+                New-ItemProperty -Path $registryPath -Name $adalkey -Value "0" -PropertyType DWORD -Force | Out-Null
+                Remove-ItemProperty -Path $registryPath -Name $versionkey
+                }
+        }
+        #Download TW to desktop
+        elseif ($answer -eq '3') {
+            $outpath = "C:\Users\" + $env:username + "\Desktop\ITRsupport.exe"
+            Invoke-WebRequest -Uri "https://www.itrelation.dk/Admin/Public/DWSDownload.aspx?File=%2fFiles%2fFiles%2fITRsupport.exe" -OutFile $outpath
+            }
+
+
+
+ } until ($answer -eq '9')
